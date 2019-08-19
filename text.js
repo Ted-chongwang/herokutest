6e3b0e9182c3e9e29098720636e5c49538e702c7
wch8812 <53140860+wch8812@users.noreply.github.com>
2019-08-19 14:05:37 +1200

    Create text.js

---
+++ text.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>hello</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+click
+</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
