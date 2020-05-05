d11088168439bfcf8030779314e88da2010bf15d
wch8812 <53140860+wch8812@users.noreply.github.com>
2020-05-05 20:01:42 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,136 +1,22 @@
-<!-- Main -->
-<div class="outer-box">
-    {% if time %}
-    <div class="item-coldown"><span>{{ time }}</span></div>
-    {% endif %}
-    <div class="inner-box">
-        <div class="item-icon float"><img src="{{ id_img }}" alt="item"></div>
-        <div class="item-name float"><span>{{ id }}</span></div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <div class="item-source">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-            <div class="item-spec float">{{ effect }}</div>
-        </div>
-    </div>
-</div>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
 
-<!-- Stylesheet -->
-<style>
-    .float {
-        float: left;
-    }
-
-    .clearfix {
-        clear: both;
-    }
-
-    .item-name,
-    .item-spec {
-        padding: 0 0 0 20px;
-    }
-
-    .item-name {
-        color: white;
-    }
-
-    .item-spec {
-        color: #c6cdd5;
-    }
-
-    .outer-box {
-        background: rgba(0, 0, 0, .7);
-        border-radius: 5px;
-    }
-
-    .outer-box,
-    .inner-box {
-        width: 100%;
-    }
-
-    .inner-box {
-        padding: 20px;
-    }
-
-    .item-icon>img {
-        width: 100%;
-        margin: 0 !important;
-    }
-
-    .item-icon {
-        width: 30%;
-    }
-
-    .item-name {
-        font-size: 20px;
-        font-weight: 600;
-        width: 60%;
-    }
-
-    .item-spec {
-        margin: 3px 0 0 0;
-        width: 70%;
-    }
-
-    .item-source {
-        width: 100%;
-    }
-
-    .item-source-icon>img {
-        margin: 0 !important;
-        width: 100%;
-    }
-
-    .item-source-icon>img:before {
-        content: '-';
-        color: white;
-    }
-
-    .item-source-box {
-        float: right;
-        width: 90%;
-        padding: 10px 0 0 0;
-    }
-
-    .item-source-icon {
-        width: 20%;
-    }
-
-    .item-source-info {
-        padding-left: 20px;
-        width: 80%;
-    }
-
-    .item-source-name,
-    .item-source-spec {
-        width: 100%;
-        font-size: 14px;
-        line-height: 20px;
-    }
-
-    .item-source-name {
-        color: #c6cdd5;
-        font-weight: 600;
-    }
-
-    .item-source-spec {
-        color: #c6cdd5;
-        font-weight: 400;
-    }
-
-    .item-coldown {
-        float: right;
-        width: 10%;
-        color: white;
-        background: black;
-        border-radius: 0 5px 0 5px;
-        text-align: center;
-    }
-        .item-coldown {
-        float: right;
-        width: 10%;
-        color: white;
-        background: black;
-        border-radius: 0 5px 0 5px;
-        text-align: center;
-    }
-</style>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
