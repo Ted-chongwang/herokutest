f0cb7d27ede699c10ccf8e2f5ea6166f6b5437b5
wch8812 <53140860+wch8812@users.noreply.github.com>
2020-05-05 20:19:23 +1200

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,20 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     
     <title>NoSQL Development: MongoDB</title>
   </head>
   <body class="main-layout">
-    <h1>Hello, world!3</h1>
+    <div>
+      class<h1>Hello, world!3</h1>
+    </div>
+    <div>
     <p>oject management. Several agile software planning tools were developed; they grow in number and complexity. Our analysis of 30 of the most known agile planning tools shows that while the majority of them support the key agile planning activities, they still have limitations. In this paper, we presented three limitations to the current agile planning tools and we developed three features to overcome them. A questionnaire is used to evaluate the identified limitations and the proposed features. The evaluation shows that the identified limitations are sound and exist in the real software projects. In addition, most of the participants agree about how to implement the first and second proposed features (providing task effort notification and improving user story prioritization). The evaluation of the third feature (adding new user story/re-prioritize user story by customer) shows that the simple implementation provided for this feature is not sufficient and, hence, there is a need for a more thorough study to best involve customers through agile planning tools. This research contributes in developing advanced agile planning tools supporting the key agile planning activities.
 Keywords—Software Development; Agile; Planning; Tools.
 I. INTRODUCTION
 Agile software adoption spreads over the world. One important aspect of agile development is the project planning. It defines the project activities that will be performed and describes how those activities will be accomplished. This includes defining: the specific work to be performed, estimates to be documented for planning, tracking and controlling the project, also, it includes project alternatives, assumptions and constraints [1]. Project planning provides benefits to agile development in many ways, such as management and communication: it reviews project progress, detects development problems and generates suitable plans to decide on the use of team resources [2]. It also, connects developers with customers and helps to reduce the misunderstandings between each other.
 Although there are many agile planning tools, it is 
     </p>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+    </div>
+      </body>
 </html>
